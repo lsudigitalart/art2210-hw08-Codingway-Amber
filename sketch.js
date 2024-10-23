ad0f7935cd9050f048aecca110b880be34ad2cd3
Codingway-Amber <amberreine7@gmail.com>
2024-10-23 14:15:51 -0500

    added size changer

--- sketch.js
+++ sketch.js
@@ -1,7 +1,21 @@
+let = generateSmol = false;
+let = smolSize 
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(500, 600);
 }
 
 function draw() {
   background(220);
+  translate(width/2, height/2);
+  cuteSmol(50);
+
+  if (generateSmol) {
+    cuteSmol(smolSize)
+  }
+
+}
+
+function cuteSmol(smolWidth) {
+  circle(0, 0, smolWidth);
 }
