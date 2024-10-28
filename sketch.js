93d29fecadd9eb6d2cd6105d165a716692f8118a
Codingway-Amber <amberreine7@gmail.com>
2024-10-28 14:03:50 -0500

    made pc face

--- sketch.js
+++ sketch.js
@@ -1,36 +1,46 @@
 function setup() {
-  createCanvas(500, 600);
+  createCanvas(500, 400);
 }
 
 function draw() {
   background(220);
-  randomSeed(0);
-  for (var i = 35; i < width + 40; i += 40) {
-  var pink = int(random(0, 102));
-  var scalar = random(0.25, 1.0);
-  cat(i, 110, pink, scalar);
- }
+  // noStroke();
 
-  push()
-  fill(0)
-  text(mouseX, 0, 240); 
-  text(mouseY, 0, 260);
-  pop()
+  push();
+  fill(0);
+  text(mouseX, 0, 370); 
+  text(mouseY, 0, 390);
+  pop();
+
+  smolBoi();
 }
 
-function cat(x, y, p, s) {
-  push()
-  translate(x, y);
-  scale(s); // Set the scale
-  stroke(p); // Set the pink value
-  strokeWeight(70);
-  line(0, -60, 0, -65); // Body
-  noStroke();
-  fill(255, 192, 203-p);
-  ellipse(-17.5, -65, 30, 30); // Left eye dome
-  ellipse(17.5, -65, 30, 30); // Right eye dome
-  fill(p);
-  ellipse(-14, -65, 8, 8); // Left eye
-  ellipse(14, -65, 8, 8); // Right eye
-  pop()
+function smolBoi() {
+  // push();
+  // noFill();
+  // // translate(245, 150)
+  // strokeWeight(1);
+  // stroke(232, 74, 116);
+  // curve(5, 26, mouseX, mouseY, 73, 61, 15, 65);
+  // pop();
+  
+  // Body
+  push();
+  strokeWeight(.7);
+  translate(19, 19);
+  scale(4);
+  fill(70);
+  rect(0, 0, 115, 75);
+  pop();
+
+  // Face
+  push();
+  strokeWeight(.7);
+  translate(48, 39);
+  scale(3.4);
+  fill(48, 169, 68);
+  rect(0, 0, 118, 75);
+  pop();
+
+
 }
